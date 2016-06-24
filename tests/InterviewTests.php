9ddca9da7ad12bcc506926a0800e8d65cbdd5bd2
Sean Straus <sstraus21@gmail.com>
2016-06-24 12:11:16 -0500

    changed dependency structure

--- tests/InterviewTests.php
+++ tests/InterviewTests.php
@@ -6,7 +6,8 @@
  * Create a class in the Vault namespace and rewrite each test to make the assertions pass.
  * NOTE: You can use any third party packages you deem necessary to complete the tests. 
  */
- require_once "Solutions.php";
+ require "../vendor/autoload.php";
+ use Vault\TestSolutions;
 
 class InterviewTests extends phpunit\framework\TestCase {
 
@@ -18,8 +19,7 @@ class InterviewTests extends phpunit\framework\TestCase {
         $data = "I want this job.";
 
         // Code here
-		$solver = new TestSolutions();
-		$data = $solver->reverseArray($data);
+		$data = Vault\TestSolutions::reverseArray($data);
 
         $this->assertEquals(['job', 'this', 'want', 'I'], $data);
     }
@@ -32,8 +32,7 @@ class InterviewTests extends phpunit\framework\TestCase {
         $data = ["200", "450", "2.5", "1", "505.5", "2"];
 
         // Code here
-		$solver = new TestSolutions();
-		$data = $solver->orderArray($data);
+		$data = Vault\TestSolutions::orderArray($data);
 
         $this->assertEquals(1, $data[0]);
         $this->assertEquals(2, $data[1]);
@@ -64,13 +63,12 @@ class InterviewTests extends phpunit\framework\TestCase {
         $data2 = [2, 4, 5, 7, 8, 9, 10];
 
         // Code here
-		$solver = new TestSolutions();
-		$data = $solver->getDiffArray($data2,$data1);
+		$data = Vault\TestSolutions::getDiffArray($data2,$data1);
 
         $this->assertEquals([8, 9, 10], $data);
 
         // Code here
-		$data = $solver->getDiffArray($data1,$data2);
+		$data = Vault\TestSolutions::getDiffArray($data1,$data2);
 
         $this->assertEquals([1, 3, 6], $data);
     }
@@ -84,8 +82,7 @@ class InterviewTests extends phpunit\framework\TestCase {
         $place2 = ['lat' => '42.1820210', 'lon' => '-88.3429465'];
 
         // Code here
-		$solver = new TestSolutions();
-		$distance = $solver->getDistance($place1,$place2);
+		$distance = Vault\TestSolutions::getDistance($place1,$place2);
 
         $this->assertEquals(36.91, $distance);
     }
@@ -100,8 +97,7 @@ class InterviewTests extends phpunit\framework\TestCase {
         $time2 = "2016-06-05T15:00:00";
 
         // Code here
-		$solver = new TestSolutions();
-		$timeDiff = $solver->getHumanTimeDiff($time2,$time1);
+		$timeDiff = Vault\TestSolutions::getHumanTimeDiff($time2,$time1);
 
         $this->assertEquals("3 hours ago", $timeDiff);
     }
