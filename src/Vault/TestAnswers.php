755ac20fffd6fcaa79e85b7dde0c35fc449b884b
George Rush <george.d.rush@gmail.com>
2017-05-03 21:48:42 -0500

    Satisfied 'testOrderArray'.

--- src/Vault/TestAnswers.php
+++ src/Vault/TestAnswers.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 namespace Vault;
 
 class TestAnswers
@@ -10,6 +11,25 @@ class TestAnswers
     // split string into array and reverse it
     $dataArray = explode(" ", $data);
     return array_reverse($dataArray);
+  }
+
+  public static function sortStringArrayNumerically(array $data): array
+  {
+    // convert values to floating-point numbers or integers as appropriate
+    for ($i = 0, $max = count($data); $i < $max; $i++)
+    {
+      if (strpos($data[$i], '.') !== false)
+      {
+        $data[$i] = (float)$data[$i];
+      }
+      else
+      {
+        $data[$i] = (int)$data[$i];
+      }
+    }
+    // sort in ascending order
+    sort($data, SORT_NUMERIC);
+    return $data;
   }
 }
 
