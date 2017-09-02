70aee35e8c704f5cbbffb99fa7c3f4f3159a749e
Daniel J. Brieck Jr <djbrieck@gmail.com>
2017-09-02 08:21:47 -0400

    Added in some comments

--- src/Interview.php
+++ src/Interview.php
@@ -4,11 +4,14 @@ namespace Vault;
 class Interview {
 
   public function reverseArray($string){
+    //Removing any punctuations from the string
     $string = preg_replace("#[[:punct:]]#","", $string);
+
+    //converting string to array
     $stringArray = explode(" ", $string);
 
+    //Reverse and return it
     $reverse = array_reverse($stringArray);
-
     return $reverse;
   }
 
