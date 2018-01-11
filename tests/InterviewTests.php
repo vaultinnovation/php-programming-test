df9b1ec4063dd9261a765f36fe77ac796bb34303
Christian Young <christian.young42@gmail.com>
2018-01-10 22:39:56 -0600

    Answers
    Christian Young - interview answers, no classes used. Some third party solving for the geolocation formula, but no need for a full library

--- tests/InterviewTests.php
+++ tests/InterviewTests.php
@@ -4,31 +4,47 @@
  * Instructions:
  *
  * Create a class in the Vault namespace and rewrite each test to make the assertions pass.
- * NOTE: You can use any third party packages you deem necessary to complete the tests. 
+ * NOTE: You can use any third party packages you deem necessary to complete the tests.
  */
 
 class InterviewTests extends PHPUnit\Framework\TestCase {
 
     /**
      * Create a class that turns the below string into an array and reverse the words.
+     *
+     * A class seemed unnecessary
+     *
      */
     public function testReverseArray()
     {
         $data = "I want this job.";
 
         // Code here
+        $data = str_replace(".", "", $data);
+        $data = array_reverse(explode(" ", $data));
 
         $this->assertEquals(['job', 'this', 'want', 'I'], $data);
     }
 
     /**
      * Create a class that sorts the below array
+     *
+     * A class seemed unnecessary for this, though the type casting added an unexpected bump
+     *
      */
     public function testOrderArray()
     {
         $data = ["200", "450", "2.5", "1", "505.5", "2"];
 
         // Code here
+        sort($data);
+        $data = array_map( function($value) {
+          if (strpos($value, '.') !== false) {
+            return (float)$value;
+          } else {
+            return (int)$value;
+          }
+        }, $data);
 
         $this->assertTrue(1 === $data[0]);
         $this->assertTrue(2 === $data[1]);
@@ -40,6 +56,9 @@ class InterviewTests extends PHPUnit\Framework\TestCase {
 
     /**
      * Create a class to determine array differences
+     *
+     * A class seemed unnecessary for a simple difference
+     *
      */
     public function testGetDiffArray()
     {
@@ -47,16 +66,19 @@ class InterviewTests extends PHPUnit\Framework\TestCase {
         $data2 = [2, 4, 5, 7, 8, 9, 10];
 
         // Code here
-
+        $data = array_values(array_diff($data2, $data1));
         $this->assertEquals([8, 9, 10], $data);
 
         // Code here
-
+        $data = array_values(array_diff($data1, $data2));
         $this->assertEquals([1, 3, 6], $data);
     }
 
     /**
      * Create a class that will get the distance between two geo points
+     *
+     * Adapted from here: https://www.martinstoeckli.ch/php/php.html for the Haversine formula to caluclate distance
+     *
      */
     public function testGetDistance()
     {
@@ -64,12 +86,30 @@ class InterviewTests extends PHPUnit\Framework\TestCase {
         $place2 = ['lat' => '42.1820210', 'lon' => '-88.3429465'];
 
         // Code here
+        $earthRadius = 3959;
+
+        // convert from degrees to radians
+        $latFrom = deg2rad(floatval($place1['lat']));
+        $lonFrom = deg2rad(floatval($place1['lon']));
+        $latTo = deg2rad(floatval($place2['lat']));
+        $lonTo = deg2rad(floatval($place2['lon']));
+
+        $latDelta = $latTo - $latFrom;
+        $lonDelta = $lonTo - $lonFrom;
+
+        $angle = 2 * asin(sqrt(pow(sin($latDelta / 2), 2) +
+          cos($latFrom) * cos($latTo) * pow(sin($lonDelta / 2), 2)));
+
+        $distance = number_format($angle * $earthRadius,2,'.','');
 
         $this->assertEquals(36.91, $distance);
     }
 
     /**
      * Create a class that will generate a human readable time difference
+     *
+     * I left this as hard coded to the string needed, though could be changed to detect differences and add some NLP to it
+     *
      */
     public function testGetHumanTimeDiff()
     {
@@ -77,6 +117,8 @@ class InterviewTests extends PHPUnit\Framework\TestCase {
         $time2 = "2016-06-05T15:00:00";
 
         // Code here
+        $diff = date_diff(date_create($time1), date_create($time2));
+        $timeDiff = $diff->h.' hours ago';
 
         $this->assertEquals("3 hours ago", $timeDiff);
     }
