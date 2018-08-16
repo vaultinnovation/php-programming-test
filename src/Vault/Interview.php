2f44ab028b3d27967764d7deace788f3073a497a
Carey Buchanan <cabuchanan4746@gmail.com>
2018-08-16 00:48:56 -0500

    Test orderArray Passed

--- src/Vault/Interview.php
+++ src/Vault/Interview.php
@@ -9,15 +9,26 @@ class Interview
 	//Create a class that turns the below string into an array and reverse the words
 	public function reverseArray($data)
 	{
-
-
         //Split:
 		$items = preg_split("/[\s.]+/", $data);
-        //Reverse the array:
-
+        //Get rid of empty elements:
         $items = array_filter($items);
+        //Reverse the array:
 		return array_reverse($items);
 
 	}
+
+    public function orderArray($data)
+    {
+
+        asort($data);
+
+        $arrlength = count($data);
+        for($x = 0; $x < $arrlength; $x++) {
+            return $data[$x];
+        }
+    }
+
+
 
 }
