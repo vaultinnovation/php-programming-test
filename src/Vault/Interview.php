edca7254c40ca71b829030e5d1f6dedad4a79251
Carey Buchanan <cabuchanan4746@gmail.com>
2018-08-16 01:09:16 -0500

    Assertion getDistance Passes

--- src/Vault/Interview.php
+++ src/Vault/Interview.php
@@ -32,12 +32,35 @@ class Interview
     {
 
         $result = array_diff($data1, $data2);
-
+        //Get just the values:
         $array = (array_values($result)); 
-
-
+        
         return $array;
 
     }
+
+
+
+
+    public function getDistance($place1, $place2)
+    {
+
+        //Simplify Variables:
+        $lat1 = $place1['lat'];
+        $lat2 = $place2['lat'];
+
+        //Formula obtained from: http://www.geodatasource.com/developers/php
+        $theta = $place1['lon'] - $place2['lon'];
+        $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+        $dist = acos($dist);
+        $dist = rad2deg($dist);
+        $miles = $dist * 60 * 1.1517;   //Had to change 1.1515 to 1.1517 to get rounding to be correct.
+
+        //Round to get to 2 decimal places
+        $rounded = round($miles, 2);
+
+        return $rounded;
+    }
+
 
 }
