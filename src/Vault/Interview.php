e8d0b820061ef3da56423b9e889dc7b8356e078e
Carey Buchanan <cabuchanan4746@gmail.com>
2018-08-16 10:06:47 -0500

    Assertion humanTime passed

--- src/Vault/Interview.php
+++ src/Vault/Interview.php
@@ -34,13 +34,10 @@ class Interview
         $result = array_diff($data1, $data2);
         //Get just the values:
         $array = (array_values($result)); 
-        
+
         return $array;
 
     }
-
-
-
 
     public function getDistance($place1, $place2)
     {
@@ -62,5 +59,80 @@ class Interview
         return $rounded;
     }
 
+    public function getHumanTimeDiff($time1, $time2)
+    {
 
+        //$time1 = "2016-06-05T12:00:00";
+        //$time2 = "2016-06-05T15:00:00";
+
+        //First Date:
+        $date1 = explode("-", $time1);
+        $year1 = $date1[0];
+        $month1 = $date1[1];
+        $date1 = explode("T", $date1[2]);
+        $day1 = $date1[0];
+        $date1 = explode(':', $date1[1]);
+        $hour1 = $date1[0];
+        $minute1 = $date1[1];
+        $second1 = $date1[2];
+
+        //Second Date:
+        $date2 = explode("-", $time2);
+        $year2 = $date2[0];
+        $month2 = $date2[1];
+        $date2 = explode("T", $date2[2]);
+        $day2 = $date2[0];
+        $date2 = explode(':', $date2[1]);
+        $hour2 = $date2[0];
+        $minute2 = $date2[1];
+        $second2 = $date2[2];
+
+        //Vars:
+        $years;
+        $strYears;
+        $months;
+        $strMonths;
+        $days;
+        $strDays;
+        $hours;
+        $strHours;
+        $minutes;
+        $strMinutes;
+        $seconds;
+        $strSeconds;
+
+        //Compute differences:
+        if (abs($year1 - $year2)> 0) {
+            $years = abs($year1 - $year2);
+            $strYears = $years . " years ";
+        }
+        if (abs($month1 - $month2)> 0) {
+            $months = abs($month1 - $month2);
+            $strMonths = $months . " months ";
+        }
+
+        if (abs($day1 - $day2) > 0) {
+            $days = $day1 - $day2;
+            $strDays = $days . " days ";
+        }
+
+        if (abs($hour1 - $hour2) > 0) {
+            $hours = abs($hour1 - $hour2);
+            $strHours = $hours . " hours ";
+        }
+
+        if (abs($minute1 - $minute2) > 0) {
+            $minutes = abs($minute1 - $minute2);
+            $strMinutes = $minutes . " minutes ";
+        }
+
+        if (abs($second1 - $econd2) > 0) {
+            $seconds = abs($second1 - $second2);
+            $strSeconds = $seconds . " seconds ";
+        }
+
+        $time = $strYears . $strMonths . $strDays . $strHours . $strMinutes . $strSeconds . "ago";
+
+        return $time;
+    }
 }
