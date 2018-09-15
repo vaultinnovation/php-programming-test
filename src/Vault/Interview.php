8b89a202e721ad6fc900819bcc45c1be12f5f361
ThingEngineer <josh.lee.campbell@gmail.com>
2018-09-15 17:51:37 -0500

    add empty Interview class in Vault namespace

---
+++ src/Vault/Interview.php
@@ -0,0 +1,15 @@
+<?php 
+namespace Vault;
+
+
+/**
+ * Interview class
+ * 
+ * Methods required for valid assertions in InterviewTests
+ * 
+ * @author     Josh Campbell <josh.lee.campbell@gmail.com>
+ */
+class Interview
+{
+       
+}
