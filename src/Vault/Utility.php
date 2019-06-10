6ee9f0b254e6641075a06cf9298f62e86b1d84f5
Natalie <lotz003@users.noreply.github.com>
2019-06-10 16:54:38 -0700

    Update Utility.php
    Completed a few more methods

--- src/Vault/Utility.php
+++ src/Vault/Utility.php
@@ -14,4 +14,36 @@ class Utility {
         return array_reverse($dataArray);
     }
     
+    /**
+     * Sorts an array
+     */
+    public function orderArray($dataArray)
+    {
+        return sort($dataArray);
+    }
+    
+    /**
+     * Determines array differences
+     */
+    public function getDiffArray($array1, $array2)
+    {
+        return array_diff($array1, $array2);
+    }
+    
+    /**
+     * Determines the distance between two geo points
+     */
+    public function getDistance($place1, $place2)
+    {
+        //
+    }
+    
+    /**
+     * Generates a human readable time difference
+     */
+    public function getHumanTimeDiff($time1, $time2)
+    {
+        //
+    }
+    
 }
